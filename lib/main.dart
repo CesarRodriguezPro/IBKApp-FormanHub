f9926549133567d90abbb4ded851e5ebf4c265f6
Cesar Rodriguez <streamlink777@live.com>
2019-09-01 22:32:57 -0400

    add icons for from page

--- lib/main.dart
+++ lib/main.dart
@@ -13,73 +13,74 @@ class MainConstructor extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-            Colors.blue[300],
-            Colors.indigo[900],
-          ],
-              begin: FractionalOffset.topLeft,
-              end: FractionalOffset.bottomRight,
-              stops: [0.0,0.9],
-              tileMode: TileMode.clamp),
-        ),
-        child: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Color(0xff000138),
-                child: Image.asset('images/logo.jpg', scale: 0.6),
-                radius: 60,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text('IBK Construction Group',
-                  style: TextStyle(
-                      color: Colors.lightBlueAccent,
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
-                child: new TextFormField(
-                  decoration: new InputDecoration(
-                    labelStyle: TextStyle(
-                        color: Colors.blueGrey[900]
-                    ),
-                    fillColor: Colors.blue[50],
-                    filled: true,
-                    icon:new Icon(
-                      Icons.account_circle,
-                      color: Colors.blueGrey[100],),
-                    labelText: 'Username',
-
-                  ),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
-                child: new TextFormField(
-                  decoration: new InputDecoration(
-                    labelStyle: TextStyle(
-
-                        color: Colors.blueGrey[900]
-                    ),
-                    fillColor: Colors.blue[50],
-                    filled: true,
-                    icon:new Icon(
-                      Icons.vpn_key,
-                      color: Colors.blueGrey[100],),
-                    labelText: 'password',
-
-                  ),
-                ),
-              ),
-
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Colors.blue[900],
+              Color(0xff000138),
             ],
+                begin: FractionalOffset.topLeft,
+                end: FractionalOffset.bottomCenter,
+                stops: [0.0,1.0],
+                tileMode: TileMode.clamp),
+          ),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Color(0xff000138),
+                  child: Image.asset('images/logo.jpg', scale: 1.0),
+                  radius: 36,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text('IBK Construction Group',
+                    style: TextStyle(
+                        color: Colors.yellow,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+                  child: new TextFormField(
+                    decoration: new InputDecoration(
+                      labelStyle: TextStyle(
+                          color: Colors.blueGrey[900]
+                      ),
+                      fillColor: Colors.blue[50],
+                      filled: true,
+                      icon:new Icon(
+                        Icons.account_circle,
+                        color: Colors.yellowAccent,),
+                      labelText: 'Username',
+
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+                  child: new TextFormField(
+                    decoration: new InputDecoration(
+                      labelStyle: TextStyle(
+
+                          color: Colors.blueGrey[900]
+                      ),
+                      fillColor: Colors.blue[50],
+                      filled: true,
+                      icon:new Icon(
+                        Icons.vpn_key,
+                        color: Colors.yellowAccent,),
+                      labelText: 'password',
+
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
